a25aa182b751353ce66ec082f3d4e8fbf2366bda
Pierce <pierceevans@me.com>
2022-12-30 13:59:37 -0600

    commit tree e7188cd131ce552eeede2eae1bcdc0fd63aca342

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    let words = (sentence.split(" "));
+    let capitalizeWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+    )
+    return (capitalizeWords.join(" "))
+  })
+  //
 }
